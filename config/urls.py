344cfba4ebb4a7eaf3dbc74b60047d23c6533a57
AvicennaJr <justfuad@protonmail.com>
2022-10-28 00:13:22 +0300

    Added Mixins

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
@@ -24,6 +24,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("admin/", admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
 
 if settings.DEBUG:
