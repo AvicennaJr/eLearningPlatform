980e84621096bb053b1e3784233e5f7368d6755b
AvicennaJr <justfuad@protonmail.com>
2022-10-28 17:04:41 +0300

    Added the ability to make course modules

--- courses/urls.py
+++ courses/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mine/', views.ManageCourseListView.as_view(), name='manage_course_list'),
-    path('create/', views.CourseCreateView.as_view(), name='course_create'),
-    path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
-    path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
+    path("mine/", views.ManageCourseListView.as_view(), name="manage_course_list"),
+    path("create/", views.CourseCreateView.as_view(), name="course_create"),
+    path("<pk>/edit/", views.CourseUpdateView.as_view(), name="course_edit"),
+    path("<pk>/delete/", views.CourseDeleteView.as_view(), name="course_delete"),
+    path(
+        "<pk>/module/",
+        views.CourseModuleUpdateView.as_view(),
+        name="course_module_update",
+    ),
 ]
--- courses/views.py
+++ courses/views.py
@@ -2,36 +2,75 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
+from .forms import ModuleFormset
 from .models import Course
+
 
 class OwnerMixin:
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
 
+
 class OwnerEditMixin:
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
+
 class OwnerCourseMixin(OwnerMixin):
     model = Course
-    fields = ['subject', 'title', 'slug', 'overview']
-    success_url = reverse_lazy('manage_course_list')
+    fields = ["subject", "title", "slug", "overview"]
+    success_url = reverse_lazy("manage_course_list")
+
 
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
-    template_name = 'courses/manage/course/form.html'
+    template_name = "courses/manage/course/form.html"
+
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
-    template_name = 'courses/manage/course/list.html'
-    permission_required = 'courses.view_course'
+    template_name = "courses/manage/course/list.html"
+    permission_required = "courses.view_course"
+
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    permission_required = 'courses.add_course'
+    permission_required = "courses.add_course"
+
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    permission_required = 'courses.change_course'
+    permission_required = "courses.change_course"
+
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
-    template_name = 'courses/manage/course/delete.html'
-    permission_required = 'courses.delete_course'
+    template_name = "courses/manage/course/delete.html"
+    permission_required = "courses.delete_course"
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = "courses/manage/module/formset.html"
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormset(instance=self.course, data=data)
+
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course, id=pk, owner=request.user)
+        return super().dispatch(request, pk)
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({"course": self.course, "formset": formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect("manage_course_list")
+        return self.render_to_response(
+            {
+                "course": self.course,
+                "formset": formset,
+            }
+        )
